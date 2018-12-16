db08106295359580fc8d56c45c660b2727536139
MDV-Rakirnd <33065969+Rakirnd@users.noreply.github.com>
2018-12-16 18:02:39 +0200

    Made the like/dislike buttons work!

--- Learn4Fun/src/app/quizzes/quizzes.component.ts
+++ Learn4Fun/src/app/quizzes/quizzes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-quizzes',
@@ -7,9 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizzesComponent implements OnInit {
 
-  constructor() { }
+  // quizzes
+  javaMasterQuiz = new Quiz('Java Master', 'Prove you are a Java Master!', 100, 0, ['Java', 'OOP', 'Design Patterns']);
+  cSharpGuruQuiz = new Quiz('C# Guru', 'Show the world you have a sharp mind!', 100, 0, ['C#', 'Web', '.Net']);
+
+  // images
+  likeImage = 'url(../../assets/images/like02.png)';
+  dislikeImage = 'url(../../assets/images/dislike.png)';
+
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  increaseLikes(quiz: Quiz) {
+    quiz.likeCount++;
+  }
+
+  increaseDislikes(quiz: Quiz) {
+    quiz.dislikeCount++;
+  }
+
+}
+
+export class Quiz {
+
+  title: string;
+  description: string;
+  likeCount: number;
+  dislikeCount: number;
+  tags: string[];
+
+  constructor(title: string, description: string, likeCount: number, dislikeCount: number, tags: string[]) {
+    this.title = title;
+    this.description = description;
+    this.likeCount = likeCount;
+    this.dislikeCount = dislikeCount;
+    this.tags = tags;
   }
 
 }
