9a2dd2bc5da1f13f8e02e461f42e839509d0029a
VasileSuciu <vasile.suciu7@gmail.com>
2018-12-27 21:38:58 +0200

    - Added Global Ranking

--- Learn4Fun/src/app/global-ranking/global-ranking.component.ts
+++ Learn4Fun/src/app/global-ranking/global-ranking.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GlobalRankingComponent implements OnInit {
 
+  loggedUser = new User("John Doe", 8, 15872, ['Java Master', 'Hardware Hero'])
+  andreea = new User("Andreea", 1, 25287, ['Design Patterns Juggler', 'OOP MVP'])
+  andrei = new User("Andrei", 2, 25002, ['OOP MVP', 'Math Geek'])
+  
+
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+
+export class User{
+  name: string;
+  position: number;
+  totalScore: number;
+  rewards: string[];
+
+  constructor(name: string, position: number, totalScore: number, rewards: string[]){
+    this.name = name;
+    this.position = position;
+    this.totalScore = totalScore;
+    this.rewards = rewards;
+  }
 }
