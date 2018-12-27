01ef7a9af745ead77b26152ec351a1feada2ed3e
VasileSuciu <vasile.suciu7@gmail.com>
2018-12-27 13:58:02 +0200

    - added basic QA and Tutorials viewing pages

--- Learn4Fun/src/app/qa/qa.component.ts
+++ Learn4Fun/src/app/qa/qa.component.ts
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QaComponent implements OnInit {
 
-  constructor() { }
+  javaArrays = new QA('Java:Array Index out of bound', 'Put link to Answer here', 20 , 3, ['Java', 'Arrays']);
+  javaScriptBtn = new QA('JS:Button does not trigger action', 'Put link to Answer here', 15, 4, ['JavaScript', 'HTML']);
+
+  likeImage = 'url(../../assets/images/like02.png)';
+  dislikeImage = 'url(../../assets/images/dislike.png)';
+
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  increaseLikes(qa: QA) {
+    qa.likeCount++;
+  }
+
+  increaseDislikes(qa: QA) {
+    qa.dislikeCount++;
+  }
+
+}
+
+export class QA {
+
+  title: string;
+  linkToAnswer: string;
+  likeCount: number;
+  dislikeCount: number;
+  tags: string[];
+
+  constructor(title: string, linkToAnswer: string, likeCount: number, dislikeCount: number, tags: string[]) {
+    this.title = title;
+    this.linkToAnswer = linkToAnswer;
+    this.likeCount = likeCount;
+    this.dislikeCount = dislikeCount;
+    this.tags = tags;
   }
 
 }
--- Learn4Fun/src/app/tutorials/tutorials.component.ts
+++ Learn4Fun/src/app/tutorials/tutorials.component.ts
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TutorialsComponent implements OnInit {
 
-  constructor() { }
+  javaTutorial = new Tutorial('Java Beginner', 'Learn Java from scratch!', 15, 2, ['Java', 'OOP']);
+
+  likeImage = 'url(../../assets/images/like02.png)';
+  dislikeImage = 'url(../../assets/images/dislike.png)';
+
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  increaseLikes(tutorial: Tutorial) {
+    tutorial.likeCount++;
+  }
+
+  increaseDislikes(tutorial: Tutorial) {
+    tutorial.dislikeCount++;
+  }
+
+}
+
+export class Tutorial {
+
+  title: string;
+  description: string;
+  likeCount: number;
+  dislikeCount: number;
+  tags: string[];
+
+  constructor(title: string, description: string, likeCount: number, dislikeCount: number, tags: string[]) {
+    this.title = title;
+    this.description = description;
+    this.likeCount = likeCount;
+    this.dislikeCount = dislikeCount;
+    this.tags = tags;
   }
 
 }
