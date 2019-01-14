1038a3f247fb52c109d0a30e26fbc6b43bd68b05
Medeea Constantinescu <constantinescu.medeea@gmail.com>
2019-01-14 23:06:52 +0200

    types of questions

---
+++ Learn4Fun/src/app/question/question.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-question',
+  templateUrl: './question.component.html',
+  styleUrls: ['./question.component.scss']
+})
+export class QuestionComponent implements OnInit {
+  questionNumber: string;
+  description: string = '';
+  submitted = false;
+  success = false;
+  selectedQuestion: string;
+  public questionTypes : Array<string> = ["FREE ANSWER", "MULTIPLE CHOICE", "FILL IN THE BLANKS"];
+ 
+  isMultipleChoice: boolean;
+  isFreeAnswer: boolean;
+  isFillBlanks: boolean;
+
+  constructor(public sanitizer: DomSanitizer, public route: ActivatedRoute, private formBuilder: FormBuilder, private router: Router) {
+    this.questionNumber = "1";
+    this.isFreeAnswer = true;
+    this.isMultipleChoice = false;
+    this.isFillBlanks = false;
+  }
+  
+  ngOnInit() {    
+    this.questionNumber = JSON.parse(localStorage.getItem('currentQuestion'));
+  }
+
+  onSubmit() {
+   
+  }
+
+  addNextQuestion(){
+    let x = JSON.parse(localStorage.getItem('currentQuestion'));
+
+    localStorage.setItem('currentQuestion', JSON.stringify(+x + 1));
+    this.questionNumber = JSON.stringify(+x + 1);
+    this.description = "";
+  }
+
+  changeQuestionTypes(s:any){
+    console.log(this.selectedQuestion);
+    
+    if(this.selectedQuestion == "FILL IN THE BLANKS")
+    {
+      this.isFillBlanks = true;
+      this.isFreeAnswer = false;
+      this.isMultipleChoice = false;
+    }
+    else if(this.selectedQuestion == "MULTIPLE CHOICE")
+    {
+      this.isFillBlanks = false;
+      this.isFreeAnswer = false;
+      this.isMultipleChoice = true;
+    }
+    else if(this.selectedQuestion == "FREE ANSWER")
+    {
+      this.isFillBlanks = false;
+      this.isFreeAnswer = true;
+      this.isMultipleChoice = false;
+    }
+  }
+}
